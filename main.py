5e2fb41d30bfd0354fc6f3e1123a315432400d70
Арсений <suvorovars@gmail.com>
2021-03-19 20:50:15 +0500

    еще одно обновление

--- main.py
+++ main.py
@@ -20,7 +20,8 @@ command = '''
 Новый класс [6] "Класс", "Пароль"
 Рассписание 
 '''
-days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday']
+days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
+days_ru = ['понедельник', 'вторник', 'среда', 'четверг', '', '']
 
 def write_msg(user_id, message):
     vk.messages.send(
@@ -103,6 +104,93 @@ for event in longpoll.listen():
                                    WHERE user_id = '{event.user_id}' ''').fetchall())[2:-2]
             update_journal(event.user_id, a, 'friday')
 
+        elif request == 'расписание понедельник':
+            mess = ''
+            group_id = get_group_id(event.user_id)
+            print(group_id)
+            rasp = cur.execute(f'''SELECT monday FROM {group_id} ''').fetchall()
+            for j in rasp:
+                print(j)
+                if str(j)[2:-3] != 'on':
+                    mess = mess + str(j)[2:-3] + '\n'
+            if mess == '':
+                write_msg(event.user_id, f'Не найдено рассписания на понедельник, возможно вы его не записали или удалили')
+            else:
+                write_msg(event.user_id, mess)
+
+        elif request == 'расписание вторник':
+            mess = ''
+            group_id = get_group_id(event.user_id)
+            print(group_id)
+            rasp = cur.execute(f'''SELECT tuesday FROM {group_id} ''').fetchall()
+            for j in rasp:
+                print(j)
+                if str(j)[2:-3] != 'on':
+                    mess = mess + str(j)[2:-3] + '\n'
+            if mess == '':
+                write_msg(event.user_id, f'Не найдено рассписания на вторник, возможно вы его не записали или удалили')
+            else:
+                write_msg(event.user_id, mess)
+
+        elif request == 'расписание среда':
+            mess = ''
+            group_id = get_group_id(event.user_id)
+            print(group_id)
+            rasp = cur.execute(f'''SELECT wednesday FROM {group_id} ''').fetchall()
+            for j in rasp:
+                print(j)
+                if str(j)[2:-3] != 'on':
+                    mess = mess + str(j)[2:-3] + '\n'
+            if mess == '':
+                write_msg(event.user_id, f'Не найдено рассписания на среду, возможно вы его не записали или удалили')
+            else:
+                write_msg(event.user_id, mess)
+
+        elif request == 'расписание четверг':
+            mess = ''
+            group_id = get_group_id(event.user_id)
+            print(group_id)
+            rasp = cur.execute(f'''SELECT thursday FROM {group_id} ''').fetchall()
+            for j in rasp:
+                print(j)
+                if str(j)[2:-3] != 'on':
+                    mess = mess + str(j)[2:-3] + '\n'
+            if mess == '':
+                write_msg(event.user_id, f'Не найдено рассписания на четверг, возможно вы его не записали или удалили')
+            else:
+                write_msg(event.user_id, mess)
+
+        elif request == 'расписание пятница':
+            mess = ''
+            group_id = get_group_id(event.user_id)
+            print(group_id)
+            rasp = cur.execute(f'''SELECT friday FROM {group_id} ''').fetchall()
+            for j in rasp:
+                print(j)
+                if str(j)[2:-3] != 'on':
+                    mess = mess + str(j)[2:-3] + '\n'
+            if mess == '':
+                write_msg(event.user_id, f'Не найдено рассписания на пятницу, возможно вы его не записали или удалили')
+            else:
+                write_msg(event.user_id, mess)
+
+        elif request == 'расписание суббота':
+            group_id = get_group_id(event.user_id)
+            mess = ''
+            a = f"""SELECT * FROM sqlite_master WHERE type = 'table' AND name = '{group_id}' AND sql LIKE '%saturday%'"""
+            if a == '':
+                write_msg(event.user_id, f'Ваше расписание работает по пятидневной системе обучения')
+                continue
+            print(group_id)
+            rasp = cur.execute(f'''SELECT saturday FROM {group_id} ''').fetchall()
+            for j in rasp:
+                print(j)
+                if str(j)[2:-3] != 'on':
+                    mess = mess + str(j)[2:-3] + '\n'
+            if mess == '':
+                write_msg(event.user_id, f'Не найдено рассписания на субботу, возможно вы его не записали или удалили')
+            else:
+                write_msg(event.user_id, mess)
 
 
         elif request == 'расписание':
